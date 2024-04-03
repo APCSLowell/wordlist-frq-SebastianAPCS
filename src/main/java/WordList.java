c6c1cbe25d53d4685066eb6b04b7d7256363a4f1
Sebastian Dowell <142616573+SebastianAPCS@users.noreply.github.com>
2024-04-03 16:38:47 +0000

    implement WordList methods

--- src/main/java/WordList.java
+++ src/main/java/WordList.java
@@ -8,15 +8,27 @@ public class WordList
   public void add(String word) { myList.add(word); }
   public String toString() { return myList.toString(); }
 
-  public int numWordsOfLength(int len)
-  {
-    /* to be implemented in part (a) */
-    
+  public int numWordsOfLength(int len) {
+    int count = 0;
+
+    for (String word : myList) {
+      if (word.length() == len) {
+        count++;
+      }
+    }
+
+    return count;
   }
 
-  public void removeWordsOfLength(int len)
-  {
-    /* to be implemented in part (b) */
+  public void removeWordsOfLength(int len) {
+    int index = 0;
 
+    for (String word : myList) {
+      if (word.length() == len) {
+        myList.remove(index);
+      } else {
+        index++;
+      }
+    }
   }
 }
